7951f99a2d7c69dbfc0b674d1f14105f0123fa27
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-03 23:37:50 +0000

    initial commit

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ from .models import BlogPost
 class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
-        fields = ["id", "title", "content", "published_dat"]
+        fields = ["id", "title", "content", "published_date"]
