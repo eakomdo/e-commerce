0c113b97119cd660b808a1a324a8dc76f05b236d
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-05 23:56:44 +0000

    added page limit

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,5 +3,13 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, Permis
 
 
 class BaseUser(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        
+        user.set_password(password)
+        
+        
     
