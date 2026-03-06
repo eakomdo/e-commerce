94861acba36dab4c20e871c318f5c10414fcc303
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-06 17:13:39 +0000

    added serializers

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -6,12 +6,27 @@ from .models import User
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     confirm_password = serializers.CharField(write_only=True)
-    
+
     class Meta:
         model = User
-        field = ['first_name', 'last_name', 'email', 'phone_number', 'password', 'confirm_password']
-        
+        field = [
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "password",
+            "confirm_password",
+        ]
+
+
 def validate(self, attrs):
-    if attrs['password'] != attrs['confirm_password']:
-        raise serializers.ValidationError('Passwords do no match')
+    if attrs["password"] != attrs["confirm_password"]:
+        raise serializers.ValidationError("Passwords do no match")
     return attrs
+
+
+def create(self, validated_data):
+    validated_data.pop("confirm_password")
+    user = User.objects.create_user(**validated_data)
+    return user
+
