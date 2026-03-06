255002906f5ac538b33f6a0ae7d75e0517491012
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-06 17:41:46 +0000

    added serializers

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate
 from .models import User
 
 
-#user registration
+# user registration
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     confirm_password = serializers.CharField(write_only=True)
@@ -32,27 +32,46 @@ def create(self, validated_data):
     return user
 
 
-#login serializer
+# login serializer
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
-    
-    
-    def validate (self, attrs):
-        email = attrs.get('email')
-        self.password = attrs.get('password')
-        
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        self.password = attrs.get("password")
+
         user = authenticate(username=email, password=password)
-        
+
         if not user:
-            raise serializers.ValidationError('Invalid Email or Password')
-        
+            raise serializers.ValidationError("Invalid Email or Password")
+
         if not user.is_verified:
-            raise serializers.ValidationError('Please verify your email first')
-        
+            raise serializers.ValidationError("Please verify your email first")
+
         if not user.is_active:
-            raise serializers.ValidationError('Your account is disabled')
-        
-        attrs['user'] = user
-        
+            raise serializers.ValidationError("Your account is disabled")
+
+        attrs["user"] = user
+
         return attrs
+    
+    #user serializer
+    class UserSerialiazer(serializers.ModelSerializer):
+        full_name = serializers.CharField(read_only=True)
+        
+        class Meta:
+            model = User
+            fields = ['id',
+            'email',
+            'first_name',
+            'last_name',
+            'full_name',
+            'phone_number',
+            'profile_picture',
+            'role',
+            'is_verified',
+            'created_at',]
+            
+            read_only_fields = ['id', 'email', 'role', 'is_verified', 'created_at']
+
