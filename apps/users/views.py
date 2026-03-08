7169907f13e3b6710b4637cf62558ffeb7b5133a
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-08 17:55:11 +0000

    writing views

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
@@ -13,3 +14,13 @@ from .serializers import (
     PasswordResetConfirm,
     PasswordResetRequestSerialiazer,
 )
+
+#generating jwt token
+def generate_jwt_token(User):
+    refresh = RefreshToken.for_user(user)
+    
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token)
+    }
+    
