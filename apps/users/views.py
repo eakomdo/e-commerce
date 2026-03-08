58d09c73023f3748bd90bf5dfb67eced21177cf4
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-08 17:40:32 +0000

    writing views

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,1 +1,15 @@
-from django_resstframework.views import APIView
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from .models import User
+from .serializers import (
+    RegistrationSerializer,
+    LoginSerializer,
+    UserSerializer,
+    PasswordResetConfirm,
+    PasswordResetRequestSerialiazer,
+)
