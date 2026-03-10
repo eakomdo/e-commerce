af843ee1aa9c7d384f2708e2f2f5da677fe26059
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-10 17:55:32 +0000

    products model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+
+#Categories Model
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = AutoSlugField(populate_from='name', always_update=False, unique=True)
+    description = models.TextField(blank=True)
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_plural_name = 'Categories'
+        
+        def __str__(self):
+            return self.tile
+        
