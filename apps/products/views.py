903c727d5729cc378355740c65a669de4bac7929
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-11 19:55:14 +0000

    filters

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
+from django.shortcuts import get_object_or_404
+from .filters import ProductFilter
+from .serializers import(
+    ProductSerialiser,
+    CategorySerializer,
+    ProductImageSerializer
+)
+from .models import Product, Category, ProductImage
 
-# Create your views here.
+
+#permission funtion
+class IsAdminOrReadOnly(AllowAny):
+    def has_permission(self, request, view):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return (request.user.is_authenticated and request.user_is_admin)
+    
+    
