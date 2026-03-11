cfd7995f04162d5609eeb00daa8939ce6d488d03
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-11 13:36:43 +0000

    products serializer

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -12,4 +12,42 @@ class ProductImageSerializer(serializers.ModelSerializer):
         model = ProductImage
         fields = ['id', 'image', 'caption']
         
+class SimpleCatrgotySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug']
+        
+class ProductSerialiser(serializers.ModelSerializer):
+    category = SimpleCatrgotySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all() , source='category', write_only=True)
+    image = ProductImageSerializer(many=True, write_only=True)
+    effective_price = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    is_in_stock = serializers.BooleanField(read_only=True)
+    discount_percentage = serializers.FloatField(read_only=True)
+    
+      class Meta:
+        model = Product
+        fields = [
+            'id',
+            'category',      
+            'category_id',     
+            'name',
+            'slug',
+            'description',
+            'price',
+            'discount_price',
+            'effective_price',  
+            'discount_percentage',  
+            'stock',
+            'is_in_stock',      
+            'image',
+            'images',           
+            'is_available',
+            'created_at',
+        ]
+        
+        
+        def validate_price(self, value):
+            if 
+    
     
