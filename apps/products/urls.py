23bade9d14b07969106db0e68dc6caff6083738c
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-13 18:19:09 +0000

    writing urls for products

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     #get single category by slug, update category (admin only), delete category (admin only)
     path('categories/<slug:slug>/', CategoryDetailView.as_view(), name='category-detail'),
 
-    #list all products (supports filtering)
+    #list all products (supports filtering), create a product (admin only)
     #
     path('', ProductListCreateView.as_view(), name='product-list'),
 
