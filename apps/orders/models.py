bb28490c6e6e5d2993ea7ee97b95b29c18f8fd3a
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-18 12:36:52 +0000

    order  model

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -3,4 +3,10 @@ from apps.users.models import User
 
 #cart model
 class Cart(models.Model):
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    @property
+    def total_item(self):
+        
