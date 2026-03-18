906922f8f56fa4d050e4cbfbe99d4920b53b607e
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-18 15:56:17 +0000

    order  model

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -77,4 +77,5 @@ class OrderItem(models.Model):
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     
-    
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name} in order #{self.order.id}'
