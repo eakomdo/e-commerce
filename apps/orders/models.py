f703e583d7254d139f2ad7b8aa569b1d494491af
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-18 15:16:16 +0000

    order  model

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -57,4 +57,11 @@ class Order(models.Model):
         shipping_country = models.CharField(max_length=20)
         shipping_city = models.CharField(max_length=20)
         notes = models.TextField(blank=True)
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
         
+        def __str__(self):
+            return f'Order of #{self.id} by {self.user.email}'
+        
+        class Meta:
+            ordering = ['-created_at']
