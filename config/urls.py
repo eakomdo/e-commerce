ea5eb32b03fb63a4c57387139b35ed40e5b1fa3a
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-18 11:18:22 +0000

    writing urls for products

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('apps.users.urls'))
+    path('api/users/', include('apps.users.urls')),
+    path('api/products', include('apps.products.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
