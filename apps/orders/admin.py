e0adf3c80ed7d60e582def15063e90a3232cd642
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-19 16:13:22 +0000

    order  model

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -4,4 +4,6 @@ from .models import Cart, CartItem, Order, OrderItem
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    
+    list_display = ['id', 'user', 'status', 'total_amount', 'created_at']
+    list_filters = ['status']
+    search_fields = ['user__email']
