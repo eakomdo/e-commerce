90cfb7c3fb1935540064dc0534ec47e8c4206ae7
Emmanuel <akomdoemmanuel22@gmail.com>
2026-03-19 16:38:08 +0000

    order serializer

--- apps/orders/serializers.py
+++ apps/orders/serializers.py
@@ -3,3 +3,5 @@ from .models import Cart, CartItem, Order, OrderItem
 from apps.products.serializers import ProductSerializer
 
 
+class CartItemSerializers(serializers.ModelSerializer):
+    product = ProductSerializer(many=True)
