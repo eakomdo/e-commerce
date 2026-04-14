c0e591b1994fc9bbc4b5f4f91e3437fa32dc193d
Emmanuel <akomdoemmanuel22@gmail.com>
2026-04-14 14:59:54 +0000

    serializer for orders

--- apps/orders/serializers.py
+++ apps/orders/serializers.py
@@ -5,3 +5,28 @@ from apps.products.serializers import ProductSerializer
 
 class CartItemSerializers(serializers.ModelSerializer):
     product = ProductSerializer(many=True)
+    
+    product_id = serializerS.PrimaryKeyRelatedField(
+        source='product',
+        read_only=True
+    )
+    
+    subtotal = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        read_only=True
+    )
+    
+    class Meta: 
+        model = CartItem
+        fields = [
+            'id',
+            'product',
+            'product_id',
+            'quantity',
+            'subtotal',
+            'added_at'
+        ]
+        read_only_fields = ['id', 'added_at']
+        
+        
