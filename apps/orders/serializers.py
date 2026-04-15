f0f63a57b6a08e701b6f8db69e8f8359137bd0eb
Emmanuel <akomdoemmanuel22@gmail.com>
2026-04-15 15:07:46 +0000

    serializer for orders

--- apps/orders/serializers.py
+++ apps/orders/serializers.py
@@ -149,6 +149,9 @@ class OrderSerializer(serializers.ModelSerializer):
 class CheckoutSerializer(serializers.ModelSerializer):
     shipping_country = serializers.CharField(max_length=100)
     shipping_city = serializers.CharField(max_length=100)
-    shipping_address = serializers.charfield(max_lenght=100)
+    shipping_address = serializers.charfield()
+    notes = serializers.CharField(required_field=False, allow_blank=True)
     
+    
+    #
     def validate(self, attrs):
