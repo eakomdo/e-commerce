35ffd131b2b7e09bc8d7c919429e5b3b5a0c5476
Emmanuel <akomdoemmanuel22@gmail.com>
2026-05-01 23:57:31 +0000

    serializer for orders

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,4 +1,4 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .serializers import CartItemSerializers, CartSerializer, AddToCartSerializer, OrderItemSerializer, OrderSerializer,CheckoutSeriali
+from .serializers import CartItemSerializers, CartSerializer, AddToCartSerializer, OrderItemSerializer, OrderSerializer,CheckoutSerializ
