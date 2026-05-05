43d0d0720ef6d95f4842cbf47bbd43e3198093ce
Emmanuel <akomdoemmanuel22@gmail.com>
2026-05-05 14:45:50 +0000

    views for orders

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -3,23 +3,60 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.db import transaction
-from .serializers import CartItemSerializers, CartSerializer, AddToCartSerializer, OrderItemSerializer, OrderSerializer,CheckoutSerializer
+from .models import Cart, CartItem, Order, OrderItem
+from .serializers import (
+    CartItemSerializers,
+    CartSerializer,
+    AddToCartSerializer,
+    OrderItemSerializer,
+    OrderSerializer,
+    CheckoutSerializer,
+)
 from apps.products.models import models
 
 
-#returns the current user's cart
+# returns the current user's cart
 class CartView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def get(self, request):
         cart, created = Cart.objects.get_or_create(request=request.user)
-        
-        serialzer = CartSerializer(cart, context={'request' : request})
+
+        serialzer = CartSerializer(cart, context={"request": request})
         return Response(serialzer.data, status=status.HTTP_200_OK)
-    
-#add to cart 
+
+
+# add to cart
 class AddToCartView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def get(self, request):
         serializer = AddToCartSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            product_id = serializer.validated_data["product_id"]
+            quantity = serializer.validated_data["quantity"]
+
+            # get the product
+            product = Product.objects.get(id=product_id)
+
+            # get or create cart for this user
+            cart, _ = Cart.objects.get_or_create(user=request.user)
+
+            # get existing cart item for this product
+            cart_item, item_created = CartItem.objects.get_or_create(
+                cart=cart, 
+                product=product, 
+                defaults={"quantity": quantity} #default is only applied when creating and ignored when item already exists 
+            )
+            
+            #
+            if not item_created:
+                cart_item.quantity +=quantity
+                cart_item.save()
+                
+            #return updated cart
+            serializer = CartItemSerializers(
+                cart,
+                context={'request': request}
+            )
