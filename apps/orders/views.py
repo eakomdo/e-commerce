3a4bf65633446c69f9506aba0e1916480ca7ac5e
Emmanuel <akomdoemmanuel22@gmail.com>
2026-05-05 14:04:45 +0000

    views for orders

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,5 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
-
+from django.db import transaction
 from .serializers import CartItemSerializers, CartSerializer, AddToCartSerializer, OrderItemSerializer, OrderSerializer,CheckoutSerializer
+from apps.products.models import models
+
+
+#returns the current user's cart
+class CartView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        cart, created = Cart.objects.get_or_create(request=request.user)
+        
+        serialzer = CartSerializer(cart, context={'request' : request})
+        return Response(serialzer.data, status=status.HTTP_200_OK)
+    
+    
