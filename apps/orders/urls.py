ba2ca8ec21df5e0ce45a2c2f49571c23598793c7
Emmanuel <akomdoemmanuel22@gmail.com>
2026-06-08 20:05:15 +0000

    urls for orders

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -11,7 +11,10 @@ from .views import (
 
 app_name ='orders'
 
-url_patterns =[
-        # GET → get current cart
+urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
+    path('cart/add/', AddToCartView.as_view(), name='cart-add'),
+    path('cart/items/<int:item_id>/', UpdateCartItemView.as_view(), name='update-cart'),
+    path('checkout/', CheckoutView.as_view(), name='checkout')
+    
 ]
