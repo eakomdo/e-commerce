2b39e1fc9c1f0f02fc951c9571b9fa843264ed17
Emmanuel <akomdoemmanuel22@gmail.com>
2026-06-09 15:20:03 +0000

    urls for orders

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -12,10 +12,25 @@ from .views import (
 app_name ='orders'
 
 urlpatterns = [
+    #get current cart
     path('cart/', CartView.as_view(), name='cart'),
+    
+    #add item to cart
     path('cart/add/', AddToCartView.as_view(), name='cart-add'),
+    
+    #update and delete cart
     path('cart/items/<int:item_id>/', UpdateCartItemView.as_view(), name='update-cart'),
+    
+    #checkout order - covert cart to order
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     
+    #view/get order 
+    path('', OrderListView.as_view(), name='')
+    
+    #get a specific order 
+    path('<int:order_id>/', OrderDetailView.as_view(), name='order-detail')
+    
+    #cancel order
+    path('<int:order_id>/cancel/', CancelOrderView.as_view(), name='cancel-order')
     
 ]
--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    #users endpoint
     path('api/users/', include('apps.users.urls')),
-    path('api/products', include('apps.products.urls'))
+    
+    #products endpoint
+    path('api/products/', include('apps.products.urls'))
+    
+    #orders endpoint
+    path('api/orders/', include('appps.orders.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
