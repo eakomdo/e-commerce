0f0928126d89ad4587c0c4a8e23b6c3c04638256
Emmanuel <akomdoemmanuel22@gmail.com>
2026-06-12 22:24:53 +0000

    payment app

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -3,6 +3,7 @@ from apps.users.models import User
 from apps.orders.models import Order
 import uuid
 
+
 #helper function to help generte a unique paymend id for each transaction
 def generate_reference():
     return f"PAY-{uuid.uuid4}"
